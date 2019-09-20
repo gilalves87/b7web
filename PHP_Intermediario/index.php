78140b221301b9c179e2c258783c93f094fbca2c
Gilberto Alves <gamartins1987@gmail.com>
2019-09-20 17:59:58 -0300

    Estudos PHP

---
+++ PHP_Intermediario/index.php
@@ -0,0 +1,17 @@
+<?php
+    echo "<p><strong> FOR </strong></p>";
+    $x = 1;
+    while($x <= 10) {
+        echo "X é igual a ".$x."<br>";
+        $x++;
+    }
+
+    echo "<hr>";
+
+    echo "<p><strong> WHILE </strong></p>";
+    for($y = 1; $y <= 10; $y++) {
+        echo "X é igual a ".$y."<br>";
+    }
+
+    echo "<hr>";
+?>
