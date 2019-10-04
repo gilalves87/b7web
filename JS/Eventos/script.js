66ed3c5e21f261fd1faa2af3d6c1d8f725e1950e
Gilberto <gilalves87@hotmail.com>
2019-10-04 00:07:15 -0300

    Mudança de Estilo

--- JS/Eventos/script.js
+++ JS/Eventos/script.js
@@ -71,3 +71,24 @@ function focou() {
 function desfocou() {
     console.log("Desfocou do campo 1")
 }
+
+function azul() {
+    limpar();
+    document.getElementById("titulo").classList.add('azul');
+}
+
+function vermelho() {
+    limpar();
+    document.getElementById("titulo").classList.add('vermelho');
+}
+
+function verde() {
+    limpar();
+    document.getElementById("titulo").classList.add('verde');
+}
+
+function limpar() {
+    document.getElementById("titulo").classList.remove('azul');
+    document.getElementById("titulo").classList.remove('vermelho');
+    document.getElementById("titulo").classList.remove('verde');
+}
